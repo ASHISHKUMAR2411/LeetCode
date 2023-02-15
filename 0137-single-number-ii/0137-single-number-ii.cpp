a6d780265e3080d01343e9f5e1afcda2b854553f
Ashish Kumar <ashishkumar24112001@gmail.com>
2023-02-15 13:11:45 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (92.21%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // Efficient Solution 
+        int ones = 0, two = 0;
+        for(auto i : nums){
+            ones = (ones ^ i) & (~two);
+            two = (two ^ i) & (~ones);
+        }
+        return ones;
         // Brute Force Complexity : O(n) time, O(n/3) space
-        map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        int n = 0;
-        for(auto i : mp){
-            if(i.second != 3){
-                n = i.first ;
-                break;
-            }
-        }
-        return n;
+        // map<int, int> mp;
+        // for(int i = 0; i < nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // int n = 0;
+        // for(auto i : mp){
+        //     if(i.second != 3){
+        //         n = i.first ;
+        //         break;
+        //     }
+        // }
+        // return n;
     }
 };
