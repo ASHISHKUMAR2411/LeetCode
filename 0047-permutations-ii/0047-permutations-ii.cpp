a84772a7a9cd05af3115ac77bbdead2374c2d85e
Ashish Kumar <ashishkumar24112001@gmail.com>
2023-04-13 15:59:40 +0530

    Time: 4 ms (90.32%), Space: 8.4 MB (96.69%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> se;
+        vector<vector<int>> se;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         do{
-            vector<int> v;
-            for(int i = 0; i < n; i++){
-                v.push_back(nums[i]);
-            }
-            se.insert(v);
+            se.push_back(nums);
         }while(next_permutation(nums.begin(), nums.end()));
-        vector<vector<int>> ans;
-        for(auto i : se){
-            vector<int> v;
-            for(int j = 0; j < i.size(); j++){
-                v.push_back(i[j]);
-            }
-            ans.push_back(v);
-        }
-        return ans;
+        return se;
     }
 };
