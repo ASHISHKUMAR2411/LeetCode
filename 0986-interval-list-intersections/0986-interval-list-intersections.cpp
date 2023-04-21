4f9c36904a9a142f6dd7f531d0fe9c13b4d41130
Ashish Kumar <ashishkumar24112001@gmail.com>
2023-04-21 19:03:25 +0530

    Time: 34 ms (66.40%), Space: 18.8 MB (47.90%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
-        set<vector<int>> ans;
-        int st = 0, en = 0;
-        for(int i =0; i < f.size(); i++){
-            for(int j = 0; j < s.size(); j++){
-                st = max(s[j][0], f[i][0]);
-                en = min(s[j][1], f[i][1]);
-                if(st >= f[i][0] and en <= f[i][1] and en >= st){
-                    vector<int> a{st, en};
-                    ans.insert(a);
-                }
+//         two pointer using interval aage jana 
+        int i = 0, j= 0;
+        vector<vector<int>> ans;
+        while(i < f.size() and j < s.size()){
+            int st = max(f[i][0], s[j][0]);
+            int en = min(f[i][1], s[j][1]);
+            if(st <= en){
+                ans.push_back({st,en});
             }
+            if(f[i][1] < s[j][1]) i++;
+            else j++;
         }
-        vector<vector<int>> res;
-        for(auto i : ans){
-            res.push_back(i);
-        }
-        return res;
+        return ans;
     }
 };
